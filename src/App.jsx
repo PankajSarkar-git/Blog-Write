1b1d990ed3dfbdd6f58610f3ff8698b3edf60720
Pankaj <pankajsarkarweb456@gmail.com>
2023-09-24 21:17:21 +0530

    Authentication service

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import React , {useState} from 'react'
+
+const App = () => {
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default App
